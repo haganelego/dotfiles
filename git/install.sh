2975b519d922703d60e208e1d428bfa091d9cb0c
haganeLego <hagane01061998@icloud.com>
2021-02-12 20:35:10 +0900

    Add install script

---
+++ git/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+echo "link gitconfig"
+ln -s ~/usr/dotfiles/git/.gitconfig ~/
+echo "link gitignore_global"
+ln -s ~/usr/dotfiles/git/.gitignore_global ~/
---
+++ tmux/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+echo "link .tmux"
+ln -s ~/usr/dotfiles/tmux/.tmux ~/
+echo "link tmux.conf"
+ln -s ~/usr/dotfiles/tmux/.tmux.conf ~/
