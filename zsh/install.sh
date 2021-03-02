4878678575501d117b478043360fc1d5ec4efbb5
haganeLego <hagane01061998@icloud.com>
2021-03-02 16:52:14 +0900

    Add zsh install.sh

---
+++ zsh/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+echo "link .zshrc"
+ln -s ~/usr/dotfiles/zsh/.zshrc ~/
+echo "link .zprofile"
+ln -s ~/usr/dotfiles/zsh/.zprofile ~/
+echo "link .zprezto/"
+ln -s ~/usr/dotfiles/zsh/.zprezto ~/
